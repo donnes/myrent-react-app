e2b36aa87b8fc0b1419510fcd8cd448b581c20b0
Donald Silveira <donaldsilveira@gmail.com>
2024-04-28 03:22:42 -0300

    Added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ export default {
   darkMode: ["class"],
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        mono: [
+          "Roboto Mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
   },
   plugins: [],
 };
