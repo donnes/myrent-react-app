83e9e3a781b761d607f968727fb70a3d1d5fcac9
Donald Silveira <donaldsilveira@gmail.com>
2024-04-28 03:21:50 -0300

    Setup TanStack Router

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <main className="flex flex-1 justify-center items-center">
-      <h1 className="text-white text-2xl text-bold">Hellow World</h1>
-    </main>
-  );
-}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,34 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <div className="relative flex min-h-screen flex-col">
+      <header className="sticky top-0 z-50 w-full border-b border-zinc-50/40 bg-zinc-50/95 backdrop-blur supports-[backdrop-filter]:bg-zinc-50/80">
+        <div className="container flex h-14 max-w-screen-2xl items-center">
+          <div className="mr-4 hidden md:flex">
+            <div className="mr-6 flex items-center space-x-2">
+              <span className="font-mono font-bold text-2xl">MyRent</span>
+            </div>
+            <nav className="flex items-center gap-4 text-sm lg:gap-6">
+              <Link
+                to="/"
+                className="transition-colors hover:text-zinc-900/80 text-zinc-900/60 [&.active]:text-zinc-900"
+              >
+                Home
+              </Link>
+              <Link
+                to="/bookings"
+                className="transition-colors hover:text-zinc-900/80 text-zinc-900/60 [&.active]:text-zinc-900"
+              >
+                My Bookings
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+});
