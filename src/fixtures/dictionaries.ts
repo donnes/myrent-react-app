3616b033116b17f2c6078654e5c7e9319d1551e5
Donald Silveira <donaldsilveira@gmail.com>
2024-04-29 19:18:10 -0300

    Added dictionaries

---
+++ src/fixtures/dictionaries.ts
@@ -0,0 +1,14 @@
+export const amenitiesDictionary = {
+  wifi: "Wi-Fi",
+  pool: "Swimming Pool",
+  parking: "Parking Available",
+  airConditioning: "Air Conditioning",
+  oceanView: "Ocean View",
+  hotTub: "Hot Tub",
+  gym: "Gym",
+  petFriendly: "Pet Friendly",
+  kitchen: "Kitchen",
+  washer: "Washer",
+  fireplace: "Fireplace",
+  privateGarden: "Private Garden",
+};
