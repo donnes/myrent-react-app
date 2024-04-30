3cc91d35be74b4b2a9ef9bdbb5e5c4dc9a129d57
Donald Silveira <donaldsilveira@gmail.com>
2024-04-30 02:01:05 -0300

    Setup React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 
@@ -12,7 +13,9 @@ const router = createRouter({ routeTree });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <QueryClientProvider client={new QueryClient()}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
