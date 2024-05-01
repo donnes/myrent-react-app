34a7ccdc217caf417377452005a77f22210d0ca9
Donald Silveira <donaldsilveira@gmail.com>
2024-05-01 19:00:02 -0300

    Added link to home on logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,14 +4,14 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <div className="relative flex min-h-screen flex-col">
-      <header className="sticky top-0 z-50 w-full border-b border-zinc-50/40 bg-zinc-50/95 backdrop-blur supports-[backdrop-filter]:bg-zinc-50/80">
+      <header className="sticky top-0 z-50 w-full border-b border-zinc-100 bg-zinc-50/95 backdrop-blur supports-[backdrop-filter]:bg-zinc-50/80">
         <div className="container flex h-14 max-w-screen-2xl items-center">
           <div className="mr-4 hidden md:flex">
-            <Link to="/">
-              <div className="mr-6 flex items-center space-x-2">
+            <div className="mr-6 flex items-center space-x-2">
+              <Link to="/">
                 <span className="font-mono text-2xl font-bold">MyRent</span>
-              </div>
-            </Link>
+              </Link>
+            </div>
             <nav className="flex items-center gap-4 text-sm lg:gap-6">
               <Link
                 to="/"
