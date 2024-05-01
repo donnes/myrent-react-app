03c9335f01e90232ac437baf85bf090390f44a3f
Donald Silveira <donaldsilveira@gmail.com>
2024-05-01 19:00:29 -0300

    Updated booking schemas

--- src/validators/booking.ts
+++ src/validators/booking.ts
@@ -1,22 +1,27 @@
 import { z } from "zod";
 
+export const EXTRA_GUESTS_FEE = 10;
+
 export const BookingSchema = z.object({
   id: z.string().uuid(),
   propertyId: z.string().uuid(),
-  startDate: z.date(),
-  endDate: z.date(),
+  dates: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
   guests: z.number().min(1).max(10),
   totalPrice: z.number(),
 });
 
 export type Booking = z.infer<typeof BookingSchema>;
 
-export const CreateBookingSchema = BookingSchema.extend({});
+export const CreateBookingSchema = BookingSchema.omit({
+  id: true,
+});
 
 export type CreateBooking = z.infer<typeof CreateBookingSchema>;
 
 export const UpdateBookingSchema = CreateBookingSchema.omit({
-  id: true,
   propertyId: true,
 }).partial();
 
